17c556fdf1fdf6721a133eeb7dc2cffd27929c9f
vykuang <vykuang92@gmail.com>
2023-10-02 17:15:29 +0800

    migrate notes to jupyter notebooks

--- klang-nb.sh
+++ klang-nb.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env sh
-. ./activate && nohup jupyter lab --no-browser &
+. /home/klang/.local/share/virtualenvs/code-practice-op8MSIhg/bin/activate && nohup jupyter lab --no-browser &
 sleep 10
-tail nohup.out | grep http
+tail nohup.out | grep localhost 
+docker compose run --rm -d --service-ports --name curiousdb db
